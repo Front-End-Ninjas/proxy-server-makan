cc1275dbe2de62f4dbceff29f873482bc4b07047
Makan Azarshahy <oro1919@gmail.com>
2018-03-05 19:40:29 -0800

    Added code to route to similar service

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
+const similar = require('./routes/similar');
+const images = require('./routes/images');
+const reviews = require('./routes/reviews');
+const description = require('./routes/description');
+const items = require('./routes/items');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'client')));
 
-// app.get('/', (req, res) => {
-//   res.status(200).send(`You got ${req.url}`);
-// });
+// app.use('/similar', similar);
+app.use('/item', items);
+// app.use('/images', images);
+// app.use('/reviews', reviews);
+// app.use('/description', description);
 
 module.exports = app;
