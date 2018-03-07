87162b22d49c15a7705b1508058cdb2925c3bcdf
Makan Azarshahy <oro1919@gmail.com>
2018-03-06 18:50:07 -0800

    Added object data for refactored routes

---
+++ server/routes/bun.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const request = require('request');
+const Promise = require('bluebird');
+const path = require('path');
+const fs = Promise.promisifyAll(require('fs'));
+const concat = require('concat');
+const bundlePath = require('./bundlePaths/bundlePaths');
+
+const superbundle = express.Router();
+
+const bundleTag = {
+  similar: null,
+  reviews: null,
+  description: null,
+  images: null,
+};
+
+superbundle.get('/', (req, res) => {
+  const promises = [];
+
+  for (let service in bundleTag) {
+    request({
+      url: `http://localhost:${bundlePath[service]}`,
+      method: 'GET',
+    }, (err, response) => {
+      const { body, headers } = response;
+      if (err) {
+        console.log(err);
+      }// else {
+      //   res.send(body);
+      // }
+      if (bundleTag[service] !== headers.etag) {
+        promises.push(fs.writeFile(`client/bundles/${service}.js`, body, (err) => {
+          bundleTag[service] = headers.etag;
+        }));
+      }
+    });
+  }
+  const similarPath = './client/bundles/similar.js';
+  const imagePath = './client/bundles/images.js';
+  const desPath = './client/bundles/description.js';
+  const revPath = './client/bundles/reviews.js';
+  const superPath = './client/bundles/super.js';
+
+  if (promises.length) {
+    console.log('KICK EVERYTHING');
+    Promise.all(promises).then(() => {
+      concat([similarPath, imagePath, desPath, revPath], superPath)
+        .then((result) => {
+          res.send(result);
+        })
+        .catch(e => res.send(e));
+    });
+  } else {
+    console.log('ALREADY UP TO DATE', __dirname);
+    res.sendFile(path.join(__dirname, '../../client/bundles/super.js'));
+  }
+});
+
+module.exports = superbundle;
