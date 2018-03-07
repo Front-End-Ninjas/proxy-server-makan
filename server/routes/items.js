25b7424ffdf036b8d03d3438f090cc54115219d5
Makan Azarshahy <oro1919@gmail.com>
2018-03-06 19:39:37 -0800

    Cleaned up code

--- server/routes/items.js
+++ server/routes/items.js
@@ -6,6 +6,7 @@ const items = express.Router();
 
 items.get('/:id/:service', (req, res) => {
   const { service } = req.params;
+  
   request({
     url: `http://localhost:${port[service]}/item${req.url}`,
     method: 'GET',
