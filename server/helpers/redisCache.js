99939b4d69f2e67a788442ce1520aea7ae2f51c4
Makan Azarshahy <oro1919@gmail.com>
2018-03-11 17:21:09 -0700

    Refactored super bundle into appropriate files. Added mocks for testing

---
+++ server/helpers/redisCache.js
@@ -0,0 +1,18 @@
+const redis = require('redis');
+
+const redisClient = redis.createClient();
+
+const checkCache = (req, res, next) => {
+  redisClient.get('bundle', (err, data) => {
+    if (err) {
+      console.log('Failed to get from Redis', err);
+      next();
+    } else if (data != null) {
+      res.send(data);
+    } else {
+      next();
+    }
+  });
+};
+
+module.exports = checkCache;
